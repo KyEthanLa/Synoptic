9fa800aa74d64db25ec6f268a63ae00ec6c24519
Ethan La <kyethanla@gmail.com>
2024-06-13 03:08:03 +0100

    update

--- loginapp/routes/villager.js
+++ loginapp/routes/villager.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.render("villager")
-})
+const villagermodel = require('../model/villager');
+
+router.get('/', function(req, res, next) {
+    let isAdmin = req.session.isAdmin;
+    let user = req.session.username;
+    let loggedIn = req.session.loggedIn;
+    let villagers = villagermodel.getVillagers();
+    if(!loggedIn){
+      res.redirect('/');
+    }
+    else{
+      res.render("villager", { 
+        title: 'PMS-Admin Users',
+        isAdmin,
+        user,
+        loggedIn,
+        villagerList: villagers
+      });
+    }
+  });
 
 module.exports = router
